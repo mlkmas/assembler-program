af35dea9be6c5def12dc211a86049b91c9bd1a70
mlkmas <malakmasarwe669@gmail.com>
2025-03-14 03:58:38 +0200

    simplify

--- src/utils.c
+++ src/utils.c
@@ -13,11 +13,7 @@ char* addFile(char *fileName, char *extenstion)
     char *dot,*fullName;
     int len=MAX_LINE_LENGTH*sizeof(char);
     fullName=handleMalloc(len);
-    //didnt mention the file name has a
-//    dot= strchr(fullName,'.');
-//    /* switch the file extenstion */
-//    if(dot !=NULL)
-//        *dot='\0';
+
     strcat(fullName, extenstion);
     return fullName;
 
